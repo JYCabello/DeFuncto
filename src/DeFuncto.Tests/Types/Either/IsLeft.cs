f894b10ac48277d46ce3c02b82e5a3d8e944cfa9
JYCabello <j.cabello.ramos@gmail.com>
2021-06-22 20:06:54 +0200

    IsLeft

---
+++ src/DeFuncto.Tests/Types/Either/IsLeft.cs
@@ -0,0 +1,22 @@
+﻿using Xunit;
+
+namespace DeFuncto.Tests.Types.Either
+{
+    public class IsLeft
+    {
+        [Fact(DisplayName = "Detects that is left correctly")]
+        public void TrueOnLeft()
+        {
+            var right = Either<string, int>.Left("banana");
+            Assert.True(right.IsLeft);
+        }
+
+
+        [Fact(DisplayName = "Detects that is not left correctly")]
+        public void FalseOnRight()
+        {
+            var right = Either<int, string>.Right("banana");
+            Assert.False(right.IsLeft);
+        }
+    }
+}
