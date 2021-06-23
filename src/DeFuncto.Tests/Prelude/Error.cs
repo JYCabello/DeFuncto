ca7c9404cb35dd660da127f9200556a6e3a6cc0b
JYCabello <j.cabello.ramos@gmail.com>
2021-06-23 10:18:03 +0200

    Error test

---
+++ src/DeFuncto.Tests/Prelude/Error.cs
@@ -0,0 +1,16 @@
+﻿using Xunit;
+using static DeFuncto.Prelude;
+
+namespace DeFuncto.Tests.Prelude
+{
+    public class Error
+    {
+        [Fact(DisplayName = "Instantiates a ResultError")]
+        public void Works()
+        {
+            var error = Error("banana");
+            Assert.True(error.ToResult<int>().IsError);
+            Assert.Equal("banana", error.ErrorValue);
+        }
+    }
+}
