12969ea5bbea37a64d20ee847eec6d08c7162812
JYCabello <j.cabello.ramos@gmail.com>
2021-06-24 10:20:58 +0200

    Run

---
+++ src/DeFuncto.Tests/Extensions/Objects/Run.cs
@@ -0,0 +1,18 @@
+﻿using DeFuncto.Extensions;
+using Xunit;
+
+namespace DeFuncto.Tests.Extensions.Objects
+{
+    public class Run
+    {
+        [Fact(DisplayName = "Runs an action and a function")]
+        public void Runs()
+        {
+            var witness = new Witness();
+            witness
+                .Run(w => w.Touch())
+                .Run(w => { w.Touch(); });
+            Assert.Equal(2, witness.TimesCalled);
+        }
+    }
+}
---
+++ src/DeFuncto.Tests/Witness.cs
@@ -0,0 +1,15 @@
+﻿using static DeFuncto.Prelude;
+
+namespace DeFuncto.Tests
+{
+    public class Witness
+    {
+        public int TimesCalled { get; private set; }
+
+        public Unit Touch()
+        {
+            TimesCalled++;
+            return unit;
+        }
+    }
+}
