1a0221309a69962a6019d59af78c7d3f665211cc
JYCabello <j.cabello.ramos@gmail.com>
2021-06-24 10:54:39 +0200

    Discard

--- src/DeFuncto/Types/Unit.cs
+++ src/DeFuncto/Types/Unit.cs
@@ -4,7 +4,7 @@
     {
         public static Unit Default { get; } = new();
         public override bool Equals(object obj) => obj is Unit;
-        public bool Equals(Unit other) => true;
+        public bool Equals(Unit _) => true;
         public override int GetHashCode() => 0;
     }
 }
