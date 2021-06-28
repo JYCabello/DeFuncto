056a078711593e9f04d431f876ced42e275e3321
JYCabello <j.cabello.ramos@gmail.com>
2021-06-28 08:16:17 +0200

    Tested Iter

--- src/DeFuncto.Tests/Types/AsyncResult/Iter.cs
+++ src/DeFuncto.Tests/Types/AsyncResult/Iter.cs
@@ -6,15 +6,21 @@ namespace DeFuncto.Tests.Types.AsyncResult
 {
     public class Iter
     {
-        [Fact(DisplayName = "Iterates on the OK side")]
+        [Fact(DisplayName = "Iterates on the OK side and skips on error")]
         public async Task OnOk()
         {
             AsyncResult<string, int> ok = "banana";
             var witness = new Witness();
 
             await ok.Iter((string _) => witness.Touch());
+            await ok.Iter((int _) => witness.Touch());
             await ok.Iter((string _) => { witness.Touch(); });
             await ok.Iter((string _) =>
+            {
+                witness.Touch();
+                return Task.CompletedTask;
+            });
+            await ok.Iter((int _) =>
             {
                 witness.Touch();
                 return Task.CompletedTask;
@@ -22,6 +28,30 @@ namespace DeFuncto.Tests.Types.AsyncResult
 
             witness.ShouldHaveBeenCalled(3);
             await ok.ShouldBeOk("banana");
+        }
+
+        [Fact(DisplayName = "Iterates on the Error side and skips on OK")]
+        public async Task OnError()
+        {
+            AsyncResult<int, string> ok = "banana";
+            var witness = new Witness();
+
+            await ok.Iter((string _) => witness.Touch());
+            await ok.Iter((int _) => witness.Touch());
+            await ok.Iter((string _) => { witness.Touch(); });
+            await ok.Iter((string _) =>
+            {
+                witness.Touch();
+                return Task.CompletedTask;
+            });
+            await ok.Iter((int _) =>
+            {
+                witness.Touch();
+                return Task.CompletedTask;
+            });
+
+            witness.ShouldHaveBeenCalled(3);
+            await ok.ShouldBeError("banana");
         }
     }
 }
