8e1c1dc5a045add2e10b1bcaef965c80d7fe2f7c
JYCabello <j.cabello.ramos@gmail.com>
2021-07-01 13:41:30 +0200

    Aggresive inlining for result

--- src/DeFuncto.Core/Types/Result.cs
+++ src/DeFuncto.Core/Types/Result.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Diagnostics.Contracts;
+using System.Runtime.CompilerServices;
 using System.Threading.Tasks;
 using DeFuncto.Extensions;
 using static DeFuncto.Prelude;
@@ -26,32 +28,53 @@ namespace DeFuncto
             IsOk = true;
         }
 
+        [Pure]
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static Result<TOk, TError> Ok(TOk right) => new(right);
+
+        [Pure]
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static Result<TOk, TError> Error(TError left) => new(left);
 
+        [Pure]
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public Result<TOk2, TError> Map<TOk2>(Func<TOk, TOk2> projection) =>
             Match(ok => projection(ok).Apply(Ok<TOk2, TError>), Error<TOk2, TError>);
 
+        [Pure]
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public Result<TOk2, TError> Select<TOk2>(Func<TOk, TOk2> projection) => Map(projection);
 
+        [Pure]
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public Result<TOk, TError2> MapError<TError2>(Func<TError, TError2> projection) =>
             Match(Ok<TOk, TError2>, error => projection(error).Apply(Error<TOk, TError2>));
 
+        [Pure]
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public Result<TOk2, TError> Bind<TOk2>(Func<TOk, Result<TOk2, TError>> binder) =>
             Match(binder, Error<TOk2, TError>);
 
+        [Pure]
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public Result<TOk, TError2> BindError<TError2>(Func<TError, Result<TOk, TError2>> binder) =>
             Match(Ok<TOk, TError2>, binder);
 
+        [Pure]
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public Result<TOkFinal, TError> SelectMany<TOkBind, TOkFinal>(
             Func<TOk, Result<TOkBind, TError>> binder,
             Func<TOk, TOkBind, TOkFinal> projection
         ) =>
             Bind(ok => binder(ok).Map(okbind => projection(ok, okbind)));
 
+        [Pure]
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public TOut Match<TOut>(Func<TOk, TOut> okProjection, Func<TError, TOut> errorProjection) =>
             IsOk ? okProjection(okValue!) : errorProjection(errorValue!);
 
+        [Pure]
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public Result<TOk, TError> Iter(Action<TOk> iterator)
         {
             if (IsOk)
@@ -59,9 +82,13 @@ namespace DeFuncto
             return this;
         }
 
+        [Pure]
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public Result<TOk, TError> Iter(Func<TOk, Unit> iterator) =>
             Iter(ok => { iterator(ok); });
 
+        [Pure]
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public Result<TOk, TError> Iter(Action<TError> iterator)
         {
             if (IsError)
@@ -69,12 +96,18 @@ namespace DeFuncto
             return this;
         }
 
+        [Pure]
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public Result<TOk, TError> Iter(Func<TError, Unit> iterator) =>
             Iter(error => { iterator(error); });
 
+        [Pure]
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public Result<TOk, TError> Iter(Func<TOk, Unit> iteratorOk, Func<TError, Unit> iteratorError) =>
             this.Apply(self => self.Match(iteratorOk, iteratorError).Apply(_ => self));
 
+        [Pure]
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public Result<TOk, TError> Iter(Action<TOk> iteratorOk, Action<TError> iteratorError)
         {
             Iter(iteratorOk);
@@ -83,9 +116,20 @@ namespace DeFuncto
 
         public Option<TOk> Option => Match(Some, _ => None);
 
+        [Pure]
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static implicit operator Result<TOk, TError>(ResultOk<TOk> resultOk) => Ok(resultOk.OkValue);
+
+        [Pure]
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static implicit operator Result<TOk, TError>(TOk ok) => Ok(ok);
+
+        [Pure]
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static implicit operator Result<TOk, TError>(ResultError<TError> resultError) => Error(resultError.ErrorValue);
+
+        [Pure]
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static implicit operator Result<TOk, TError>(TError error) => Error(error);
     }
 
@@ -96,6 +140,8 @@ namespace DeFuncto
         public ResultOk(TOk okValue) =>
             OkValue = okValue;
 
+        [Pure]
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public Result<TOk, TError> ToResult<TError>() => this;
     }
 
@@ -106,24 +152,42 @@ namespace DeFuncto
         public ResultError(TError errorValue) =>
             ErrorValue = errorValue;
 
+        [Pure]
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public Result<TOk, TError> ToResult<TOk>() => this;
     }
 
     public static class ResultExtensions
     {
+        [Pure]
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static TOut Collapse<TOut>(this Result<TOut, TOut> result) => result.Match(Id, Id);
+
+        [Pure]
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static AsyncResult<TOk, TError> Async<TOk, TError>(this Task<Result<TOk, TError>> self) => self;
+
+        [Pure]
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static AsyncResult<TOk, TError> Async<TOk, TError>(this Result<TOk, TError> self) => self;
 
+        [Pure]
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static AsyncResult<TOk, TError> Async<TOk, TError>(this Result<TOk, Task<TError>> self) =>
             self.Match(ok => ok.Apply(Ok<TOk, TError>).Async(), errTsk => errTsk.Map(Error<TOk, TError>));
 
+        [Pure]
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static AsyncResult<TOk, TError> Async<TOk, TError>(this Result<Task<TOk>, TError> self) =>
             self.Match(okTsk => okTsk.Map(Ok<TOk, TError>).Async(), error => error.Apply(Error<TOk, TError>));
 
+        [Pure]
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static AsyncResult<TOk, TError> Async<TOk, TError>(this Result<Task<TOk>, Task<TError>> self) =>
             self.Match(okTsk => okTsk.Map(Ok<TOk, TError>), errTsk => errTsk.Map(Error<TOk, TError>));
 
+        [Pure]
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static AsyncResult<TOk, TError> Async<TOk, TError>(this Task<Result<Task<TOk>, Task<TError>>> self) =>
             self.Map(r => r.Async().Result());
     }
