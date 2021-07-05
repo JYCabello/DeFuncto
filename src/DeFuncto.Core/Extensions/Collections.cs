e80390cd86c81c2f598a657c98080b93be5f4b3e
JYCabello <j.cabello.ramos@gmail.com>
2021-07-05 11:48:27 +0200

    Cleanup

--- src/DeFuncto.Core/Extensions/Collections.cs
+++ src/DeFuncto.Core/Extensions/Collections.cs
@@ -13,7 +13,6 @@ namespace DeFuncto.Extensions
         public static IEnumerable<T> Choose<T>(this IEnumerable<Option<T>> self) =>
             self.Where(opt => opt.IsSome).Select(opt => opt.Value!);
 
-
         [Pure]
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static IEnumerable<TOut> Choose<T, TOut>(this IEnumerable<T> self, Func<T, Option<TOut>> f) =>
