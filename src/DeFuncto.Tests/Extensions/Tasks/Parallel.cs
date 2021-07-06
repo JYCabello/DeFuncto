590783f500ba28f4238174f648b2887294a6d032
JYCabello <j.cabello.ramos@gmail.com>
2021-07-06 12:10:24 +0200

    Disposing of the semaphore in parallel

--- src/DeFuncto.Tests/Extensions/Tasks/Parallel.cs
+++ src/DeFuncto.Tests/Extensions/Tasks/Parallel.cs
@@ -13,22 +13,30 @@ namespace DeFuncto.Tests.Extensions.Tasks
         [Theory(DisplayName = "Executes actions in parallel")]
         [InlineData(10, 2)]
         [InlineData(10, 25)]
+        [InlineData(50, 50)]
         [InlineData(100, 10)]
+        [InlineData(100, 150)]
+        [InlineData(300, 150)]
         [InlineData(850, 150)]
         [InlineData(5_000, 100)]
+        [InlineData(5_000, 300)]
         public async Task InParallel(int total, int parallelism)
         {
             var witness = new ConcurrentWitness();
 
-            await Enumerable.Range(0, total)
-                .Select<int, Func<Task<Unit>>>(_ => async () =>
+            var results = await Enumerable.Range(0, total)
+                .Select<int, Func<Task<int>>>(_ => async () =>
                 {
                     using (witness.Grab())
                     {
-                        await Task.Delay(TimeSpan.FromTicks(1));
-                        return unit;
+                        var timesCalled = witness.TimesCalled;
+                        await Task.Delay(TimeSpan.FromMilliseconds(1 + timesCalled % 25));
+                        return timesCalled;
                     }
                 }).Parallel(parallelism);
+
+            var sum = results.Sum();
+            Assert.True(total <= parallelism ? sum == 0 : total < sum);
 
             witness
                 .ShouldBeenHeldMax(parallelism)
