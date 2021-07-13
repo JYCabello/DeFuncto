6cb6c791e7b5c02ad68786f417581e9566e4494d
JYCabello <j.cabello.ramos@gmail.com>
2021-07-13 08:34:31 +0200

    More!

--- src/DeFuncto.Tests/Core/Types/Result/Linq.cs
+++ src/DeFuncto.Tests/Core/Types/Result/Linq.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using DeFuncto.Assertions;
+using FsCheck;
+using FsCheck.Xunit;
 using Xunit;
 using static DeFuncto.Prelude;
 
@@ -7,45 +9,65 @@ namespace DeFuncto.Tests.Core.Types.Result
 {
     public class Linq
     {
-        [Fact(DisplayName = "Carries along all values")]
-        public void AllOk() =>
+        [Property(DisplayName = "Carries along all values")]
+        public void AllOk(
+            NonNull<string> a,
+            NonNull<string> b,
+            NonNull<string> c,
+            NonNull<string> d,
+            NonNull<string> e,
+            NonNull<string> f) =>
         (
-            from ok1 in Ok<string, int>("b")
-            from ok2 in Ok<string, int>("a")
-            from ok3 in Ok<string, int>("n")
-            from ok4 in Ok<string, int>("a")
-            from ok5 in Ok<string, int>("n")
-            from ok6 in Ok<string, int>("a")
+            from ok1 in Ok<string, int>(a.Get)
+            from ok2 in Ok<string, int>(b.Get)
+            from ok3 in Ok<string, int>(c.Get)
+            from ok4 in Ok<string, int>(d.Get)
+            from ok5 in Ok<string, int>(e.Get)
+            from ok6 in Ok<string, int>(f.Get)
             select $"{ok1}{ok2}{ok3}{ok4}{ok5}{ok6}"
-        ).ShouldBeOk("banana");
+        ).ShouldBeOk($"{a.Get}{b.Get}{c.Get}{d.Get}{e.Get}{f.Get}");
 
-        [Fact(DisplayName = "Gets the first error found")]
-        public void GetsFirstError() =>
+        [Property(DisplayName = "Gets the first error found")]
+        public void GetsFirstError(
+            NonNull<string> a,
+            NonNull<string> b,
+            NonNull<string> c,
+            NonNull<string> d,
+            NonNull<string> e,
+            NonNull<string> f,
+            int g) =>
         (
-            from ok1 in Ok<string, int>("na")
-            from ok2 in Ok<string, int>("na")
-            from ok3 in Ok<string, int>("na")
-            from ok4 in Error<string, int>(42)
-            from ok5 in Ok<string, int>("na")
-            from ok6 in Ok<string, int>("na")
-            from ok7 in Ok<string, int>("batman")
+            from ok1 in Ok<string, int>(a.Get)
+            from ok2 in Ok<string, int>(b.Get)
+            from ok3 in Ok<string, int>(c.Get)
+            from ok4 in Error<string, int>(g)
+            from ok5 in Ok<string, int>(d.Get)
+            from ok6 in Ok<string, int>(e.Get)
+            from ok7 in Ok<string, int>(f.Get)
             select $"{ok1} {ok2} {ok3} {ok4} {ok5} {ok6} {ok7}!"
-        ).ShouldBeError(42);
+        ).ShouldBeError(g);
 
-        [Fact(DisplayName = "Stops running after first error")]
-        public void ShortCircuit()
+        [Property(DisplayName = "Stops running after first error")]
+        public void ShortCircuit(
+            NonNull<string> a,
+            NonNull<string> b,
+            NonNull<string> c,
+            NonNull<string> d,
+            NonNull<string> e,
+            NonNull<string> f,
+            int g)
         {
             (
-                from ok1 in Ok<string, int>("na")
-                from ok2 in Ok<string, int>("na")
-                from ok3 in Ok<string, int>("na")
-                from ok4 in Error<string, int>(42)
+                from ok1 in Ok<string, int>(a.Get)
+                from ok2 in Ok<string, int>(b.Get)
+                from ok3 in Ok<string, int>(c.Get)
+                from ok4 in Error<string, int>(g)
                 let boom = Boom()
-                from ok5 in Ok<string, int>("na")
-                from ok6 in Ok<string, int>("na")
-                from ok7 in Ok<string, int>("batman")
+                from ok5 in Ok<string, int>(d.Get)
+                from ok6 in Ok<string, int>(e.Get)
+                from ok7 in Ok<string, int>(f.Get)
                 select $"{ok1} {ok2} {ok3} {ok4} {ok5} {ok6} {ok7}!"
-            ).ShouldBeError(42);
+            ).ShouldBeError(g);
 
             int Boom() => throw new Exception("Should not happen");
         }
