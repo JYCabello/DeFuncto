926eb2bbe5c904863bf3343e227b2c6e512c30be
Rheijden <rheijdendeveloper@gmail.com>
2021-07-13 09:02:23 +0200

    Update RunAsync.cs
    Add pragma to lambda

--- src/DeFuncto.Tests/Core/Extensions/Tasks/RunAsync.cs
+++ src/DeFuncto.Tests/Core/Extensions/Tasks/RunAsync.cs
@@ -7,14 +7,32 @@ namespace DeFuncto.Tests.Core.Extensions.Tasks
 {
     public class RunAsync
     {
-
         [Fact(DisplayName = "Runs an asynchronous action")]
         public async Task Runs()
         {
             var witness = new Witness();
-            await witness.RunAsync(async w => w.Touch());
-            await witness.RunAsync(async w => { w.Touch(); });
-            await witness.ToTask().RunAsync(async w => { w.Touch(); });
+            await witness.RunAsync(
+#pragma warning disable 1998
+                async w => w.Touch()
+#pragma warning restore 1998
+                );
+
+            await witness.RunAsync(
+#pragma warning disable 1998
+                async w =>
+            {
+                w.Touch();
+            }
+#pragma warning restore 1998
+            );
+            await witness.ToTask().RunAsync(
+#pragma warning disable 1998
+                async w =>
+                {
+                    w.Touch();
+                }
+#pragma warning restore 1998
+                );
             Assert.Equal(3, witness.TimesCalled);
         }
     }
