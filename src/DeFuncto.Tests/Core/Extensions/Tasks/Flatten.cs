9217f47aaa2479a579ec8f71619c3f3288a42240
Rheijden <rheijdendeveloper@gmail.com>
2021-07-15 09:38:29 +0200

    FlattensArray Green

--- src/DeFuncto.Tests/Core/Extensions/Tasks/Flatten.cs
+++ src/DeFuncto.Tests/Core/Extensions/Tasks/Flatten.cs
@@ -1,4 +1,5 @@
-﻿using System.Threading.Tasks;
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
 using DeFuncto.Extensions;
 using Xunit;
 
@@ -12,6 +13,20 @@ namespace DeFuncto.Tests.Core.Extensions.Tasks
             var nestedTask = "banana".ToTask().ToTask();
             var result = await nestedTask.Flatten();
             Assert.Equal("banana", result);
+        }
+
+        [Fact(DisplayName = "Flattens Array")]
+        public async Task FlattensArray()
+        {
+            var multipleLists = new[]
+            {
+                new List<int> { 1, 2, 3, 4, 5 }.ToTask(),
+                new List<int> { 1, 2, 3, 4, 5 }.ToTask(),
+            }.Apply(Task.WhenAll);
+
+            var singleList = await multipleLists.Flatten();
+
+            Assert.Equal(10, singleList.Count);
         }
     }
 }
