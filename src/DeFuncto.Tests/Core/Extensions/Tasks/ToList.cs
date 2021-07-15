fab7d24b41037dd72a2774856dc123994ff4c456
Rheijden <rheijdendeveloper@gmail.com>
2021-07-15 08:57:56 +0200

    To List green

---
+++ src/DeFuncto.Tests/Core/Extensions/Tasks/ToList.cs
@@ -0,0 +1,24 @@
+﻿using DeFuncto.Extensions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace DeFuncto.Tests.Core.Extensions.Tasks
+{
+    public class ToList
+    {
+        [Fact]
+        public async Task FromTask()
+        {
+            var items = await GetItems().ToTask().ToList();
+
+            Assert.IsType<List<int>>(items);
+
+            IEnumerable<int> GetItems() =>
+                new List<int> { 1, 2, 3, 4, 5 };
+        }
+    }
+}
