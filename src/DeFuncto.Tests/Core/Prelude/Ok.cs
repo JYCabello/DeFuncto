6fdedb99601bed78e07378fcb750a8113980fc68
Rheijden <rheijdendeveloper@gmail.com>
2021-07-16 17:15:14 +0200

    cleaning

--- src/DeFuncto.Tests/Core/Prelude/Ok.cs
+++ src/DeFuncto.Tests/Core/Prelude/Ok.cs
@@ -1,17 +1,18 @@
 ﻿using DeFuncto.Assertions;
-using Xunit;
+using FsCheck;
+using FsCheck.Xunit;
 using static DeFuncto.Prelude;
 
 namespace DeFuncto.Tests.Core.Prelude
 {
     public class Ok
     {
-        [Fact(DisplayName = "Instantiates a ResultOk")]
-        public void Works() =>
+        [Property(DisplayName = "Instantiates a ResultOk")]
+        public void Works(NonNull<string> a) =>
             Ok("banana").Result<int>().ShouldBeOk("banana");
 
-        [Fact(DisplayName = "Instantiates a result that is Ok")]
-        public void WorksWithBoth() =>
+        [Property(DisplayName = "Instantiates a result that is Ok")]
+        public void WorksWithBoth(NonNull<string> a) =>
             Ok<string, int>("banana").ShouldBeOk("banana");
     }
 }
