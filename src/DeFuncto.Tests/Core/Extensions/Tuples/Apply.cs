9fd778f60dd4ff307c465d8ad683e9c2d64f57c2
Rheijden <rheijdendeveloper@gmail.com>
2021-07-16 16:52:11 +0200

    Apply uses Property

--- src/DeFuncto.Tests/Core/Extensions/Tuples/Apply.cs
+++ src/DeFuncto.Tests/Core/Extensions/Tuples/Apply.cs
@@ -1,38 +1,39 @@
 ﻿using DeFuncto.Extensions;
+using FsCheck.Xunit;
 using Xunit;
 
 namespace DeFuncto.Tests.Core.Extensions.Tuples
 {
     public class Apply
     {
-        [Fact(DisplayName = "Applies to tuple")]
-        public void Tuple() =>
-            ("ban", "ana")
-            .Apply((a, b) => $"{a}{b}")
-            .Run(result => Assert.Equal("banana", result));
+        [Property(DisplayName = "Applies to tuple")]
+        public void Tuple(string iA, string iB) =>
+            (iA, iB)
+            .Apply((a, b) => a + b)
+            .Run(result => Assert.Equal(iA + iB, result));
 
-        [Fact(DisplayName = "Applies to triple")]
-        public void Triple() =>
-            ("ba", "na", "na")
-            .Apply((a, b, c) => $"{a}{b}{c}")
-            .Run(result => Assert.Equal("banana", result));
+        [Property(DisplayName = "Applies to triple")]
+        public void Triple(string iA, string iB, string iC) =>
+            (iA, iB, iC)
+            .Apply((a, b, c) => a + b + c)
+            .Run(result => Assert.Equal(iA + iB + iC, result));
 
-        [Fact(DisplayName = "Applies to quadruple")]
-        public void Quadruple() =>
-            ("ba", "na", "n", "a")
-            .Apply((a, b, c, d) => $"{a}{b}{c}{d}")
-            .Run(result => Assert.Equal("banana", result));
+        [Property(DisplayName = "Applies to quadruple")]
+        public void Quadruple(string iA, string iB, string iC, string iD) =>
+            (iA, iB, iC, iD)
+            .Apply((a, b, c, d) => a + b + c + d)
+            .Run(result => Assert.Equal(iA + iB + iC + iD, result));
 
-        [Fact(DisplayName = "Applies to quintuple")]
-        public void Quintuple() =>
-            ("ba", "n", "a", "n", "a")
-            .Apply((a, b, c, d, e) => $"{a}{b}{c}{d}{e}")
-            .Run(result => Assert.Equal("banana", result));
+        [Property(DisplayName = "Applies to quintuple")]
+        public void Quintuple(string iA, string iB, string iC, string iD, string iE) =>
+            (iA, iB, iC, iD, iE)
+            .Apply((a, b, c, d, e) => a + b + c + d + e)
+            .Run(result => Assert.Equal(iA + iB + iC + iD + iE, result));
 
-        [Fact(DisplayName = "Applies to sextuple")]
-        public void Sextuple() =>
-            ("b", "a", "n", "a", "n", "a")
-            .Apply((a, b, c, d, e, f) => $"{a}{b}{c}{d}{e}{f}")
-            .Run(result => Assert.Equal("banana", result));
+        [Property(DisplayName = "Applies to sextuple")]
+        public void Sextuple(string iA, string iB, string iC, string iD, string iE, string iF) =>
+            (iA, iB, iC, iD, iE, iF)
+            .Apply((a, b, c, d, e, f) => a + b + c + d + e + f)
+            .Run(result => Assert.Equal(iA + iB + iC + iD + iE + iF, result));
     }
 }
