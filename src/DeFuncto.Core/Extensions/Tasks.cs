42a181c9fdc385d03c6da3215434a8a336ea5c90
Rheijden <rheijdendeveloper@gmail.com>
2021-07-16 08:23:48 +0200

    Implement Pure

--- src/DeFuncto.Core/Extensions/Tasks.cs
+++ src/DeFuncto.Core/Extensions/Tasks.cs
@@ -74,14 +74,17 @@ namespace DeFuncto.Extensions
         public static Task<T> RunAsync<T>(this T self, Func<T, Task<Unit>> f) =>
             self.RunAsync(async t => { await f(t); });
 
+        [Pure]
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static Task<IEnumerable<TOut>> Select<TIn, TOut>(this Task<IEnumerable<TIn>> self, Func<TIn, TOut> func) =>
             self.Map(ienumerable => ienumerable.Select(func));
 
+        [Pure]
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static Task<IEnumerable<TResult>> Select<TIn, TResult>(this Task<TIn[]> self, Func<TIn, TResult> mapper) =>
             self.Map(x => x.Select(mapper));
 
+        [Pure]
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static Task<List<T>> ToList<T>(this Task<IEnumerable<T>> self) =>
             self.Map(t => t.ToList());
