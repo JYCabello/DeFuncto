f4008927696d6fb025649334fe42c134fe466ff1
Rheijden <rheijdendeveloper@gmail.com>
2021-07-19 08:51:17 +0200

    Green

--- src/DeFuncto.Tests/Core/Types/AsyncOption/Linq.cs
+++ src/DeFuncto.Tests/Core/Types/AsyncOption/Linq.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using DeFuncto.Assertions;
 using FsCheck.Xunit;
-using Xunit;
 using static DeFuncto.Prelude;
 
 namespace DeFuncto.Tests.Core.Types.AsyncOption
@@ -17,22 +16,22 @@ namespace DeFuncto.Tests.Core.Types.AsyncOption
                 select (a + b + c))
             .ShouldBeSome(x + y + z);
 
-        [Fact(DisplayName = "Stops at one none")]
-        public void StopsNone() => (
-                from a in Some("ba").Async()
+        [Property(DisplayName = "Stops at one none")]
+        public void StopsNone(string x, string y) => (
+                from a in Some(x).Async()
                 from b in None.Option<string>().Async()
                 let error = Boom()
-                from c in Some("na").Async()
-                where c == "na"
+                from c in Some(y).Async()
+                where c == y
                 select $"{a}{b}{c}")
             .ShouldBeNone();
 
-        [Fact(DisplayName = "Filters out")]
-        public void FiltersOut() => (
-                from a in Some("ba").Async()
-                from b in Some("na").Async()
-                from c in Some("na").Async()
-                where c == "pear"
+        [Property(DisplayName = "Filters out")]
+        public void FiltersOut(string w, string x, string y, string z) => (
+                from a in Some(x).Async()
+                from b in Some(y).Async()
+                from c in Some(z).Async()
+                where c == w
                 select $"{a}{b}{c}")
             .ShouldBeNone();
 
