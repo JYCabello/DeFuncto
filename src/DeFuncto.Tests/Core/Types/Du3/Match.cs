aff5dd8ca591ca71ec05910d9631632fbf1c1ca4
R. van der Heijden <rheijdendeveloper@gmail.com>
2021-07-19 21:28:33 +0200

    Update Match.cs

--- src/DeFuncto.Tests/Core/Types/Du3/Match.cs
+++ src/DeFuncto.Tests/Core/Types/Du3/Match.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using DeFuncto.Extensions;
+using FsCheck;
+using FsCheck.Xunit;
 using Xunit;
 using static DeFuncto.Prelude;
 
@@ -7,28 +9,28 @@ namespace DeFuncto.Tests.Core.Types.Du3
 {
     public class Match
     {
-        [Fact(DisplayName = "Matches the first option")]
-        public void OnFirst() =>
-            First<string, int, DateTime>("ban").Match(
-                val => $"{val}ana",
-                val => $"{val}ana",
-                val => $"{val}ana"
-            ).Run(result => Assert.Equal("banana", result));
+        [Property(DisplayName = "Matches the first option")]
+        public void OnFirst(NonNull<string> a, NonNull<string> b) =>
+            First<string, int, DateTime>(a.Get).Match(
+                val => $"{val}{b.Get}",
+                val => $"{val}{b.Get}",
+                val => $"{val}{b.Get}"
+            ).Run(result => Assert.Equal(a.Get + b.Get, result));
 
-        [Fact(DisplayName = "Matches the second option")]
-        public void OnSecond() =>
-            Second<int, string, DateTime>("ban").Match(
-                val => $"{val}ana",
-                val => $"{val}ana",
-                val => $"{val}ana"
-            ).Run(result => Assert.Equal("banana", result));
+        [Property(DisplayName = "Matches the second option")]
+        public void OnSecond(NonNull<string> a, NonNull<string> b) =>
+            Second<int, string, DateTime>(a.Get).Match(
+               val => $"{val}{b.Get}",
+                val => $"{val}{b.Get}",
+                val => $"{val}{b.Get}"
+            ).Run(result => Assert.Equal(a.Get + b.Get, result));
 
-        [Fact(DisplayName = "Matches the third option")]
-        public void OnThird() =>
-            Third<int, DateTime, string>("ban").Match(
-                val => $"{val}ana",
-                val => $"{val}ana",
-                val => $"{val}ana"
-            ).Run(result => Assert.Equal("banana", result));
+        [Property(DisplayName = "Matches the third option")]
+        public void OnThird(NonNull<string> a, NonNull<string> b) =>
+            Third<int, DateTime, string>(a.Get).Match(
+                val => $"{val}{b.Get}",
+                val => $"{val}{b.Get}",
+                val => $"{val}{b.Get}"
+            ).Run(result => Assert.Equal(a.Get + b.Get, result));
     }
 }
