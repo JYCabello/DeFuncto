82b95bf254a560ca5399bee9b91f9246385a4226
R. van der Heijden <rheijdendeveloper@gmail.com>
2021-07-19 20:57:28 +0200

    Update Iter.cs

--- src/DeFuncto.Tests/Core/Types/AsyncResult/Iter.cs
+++ src/DeFuncto.Tests/Core/Types/AsyncResult/Iter.cs
@@ -1,33 +1,35 @@
 ﻿using System.Threading.Tasks;
 using DeFuncto.Assertions;
+using FsCheck;
+using FsCheck.Xunit;
 using Xunit;
 
 namespace DeFuncto.Tests.Core.Types.AsyncResult
 {
     public class Iter
     {
-        [Fact(DisplayName = "Iterates on the OK side and skips on error")]
-        public async Task OnOk()
+        [Property(DisplayName = "Iterates on the OK side and skips on error")]
+        public void OnOk(NonNull<string> a)
         {
-            AsyncResult<string, int> ok = "banana";
+            AsyncResult<string, int> ok = a.Get;
             var witness = new Witness();
 
-            await ok.Iter((string _) => witness.Touch());
-            await ok.Iter((int _) => witness.Touch());
-            await ok.Iter((string _) => { witness.Touch(); });
-            await ok.Iter((string _) =>
+            _ = ok.Iter((string _) => witness.Touch()).Result;
+            _ = ok.Iter((int _) => witness.Touch()).Result;
+            _ = ok.Iter((string _) => { witness.Touch(); }).Result;
+            _ = ok.Iter((string _) =>
             {
                 witness.Touch();
                 return Task.CompletedTask;
-            });
-            await ok.Iter((int _) =>
+            }).Result;
+            _ = ok.Iter((int _) =>
             {
                 witness.Touch();
                 return Task.CompletedTask;
-            });
+            }).Result;
 
             witness.ShouldHaveBeenTouched(3);
-            await ok.ShouldBeOk("banana");
+            _ = ok.ShouldBeOk(a.Get).Result;
         }
 
         [Fact(DisplayName = "Iterates on the Error side and skips on OK")]
