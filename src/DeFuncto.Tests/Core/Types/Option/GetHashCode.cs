0bdc308a15c7698c8f3d84c12dfdc78b69f1aad4
R. van der Heijden <rheijdendeveloper@gmail.com>
2021-09-20 14:57:37 +0200

    something

---
+++ src/DeFuncto.Tests/Core/Types/Option/GetHashCode.cs
@@ -0,0 +1,28 @@
+﻿using FsCheck;
+using Xunit;
+using static DeFuncto.Prelude;
+using FsCheck.Xunit;
+
+namespace DeFuncto.Tests.Core.Types.Option
+{
+    public class GetHashCode
+    {
+        [Property(DisplayName = "Some are equal")]
+        public void SomeEqual(NonNull<string> a)
+        {
+            Assert.Equal(
+                Some(a.Get).GetHashCode(),
+                Some(a.Get).GetHashCode()
+            );
+        }
+
+        [Property(DisplayName = "Some are not the same")]
+        public void SomeAreNotTheSame(NonNull<string> a)
+        {
+            Assert.NotEqual(
+                Some($"a{a.Get}").GetHashCode(),
+                Some($"b{a.Get}").GetHashCode()
+            );
+        }
+    }
+}
