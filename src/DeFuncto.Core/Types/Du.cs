2827bfdf6fd1ba55c8013548c0410f87fd314f20
R. van der Heijden <rheijdendeveloper@gmail.com>
2021-09-20 09:33:17 +0200

    Update Du.cs

--- src/DeFuncto.Core/Types/Du.cs
+++ src/DeFuncto.Core/Types/Du.cs
@@ -4,7 +4,7 @@ using System.Runtime.CompilerServices;
 
 namespace DeFuncto
 {
-    public readonly struct Du<T1, T2>
+    public readonly struct Du<T1, T2> : IEquatable<Du<T1, T2>>
     {
         public enum DiscriminationValue
         {
@@ -47,6 +47,27 @@ namespace DeFuncto
         [Pure]
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static Du<T1, T2> Second(T2 t2) => t2;
+
+        public override bool Equals(object obj)
+        {
+            var nval = obj as Du<T1, T2>?;
+            return nval == null
+                ? false
+                : Equals((Du<T1, T2>)nval);
+        }
+
+        bool IEquatable<Du<T1, T2>>.Equals(Du<T1, T2> other)
+        {
+            if (Discriminator != other.Discriminator)
+                return false;
+            return Match(ex => ex.Equals(other.t1), val => val.Equals(other.t2));
+            //return Discriminator switch
+            //{
+            //    DiscriminationValue.T1 => other.t1!.Equals(t1),
+            //    DiscriminationValue.T2 => other.t2!.Equals(t2),
+            //    _ => throw new ArgumentException(nameof(Discriminator))
+            //};
+        }
 
         [Pure]
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
