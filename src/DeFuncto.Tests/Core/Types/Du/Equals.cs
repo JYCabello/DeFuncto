02db1bd19fe9f384aa3ed826b2f7798417cf8808
R. van der Heijden <rheijdendeveloper@gmail.com>
2021-09-20 10:50:55 +0200

    Update Equals.cs
    Replaces T1 with First and T2 with Second

--- src/DeFuncto.Tests/Core/Types/Du/Equals.cs
+++ src/DeFuncto.Tests/Core/Types/Du/Equals.cs
@@ -16,48 +16,48 @@ namespace DeFuncto.Tests.Core.Types.Du
                 .Run(result => Assert.False(result));
         }
 
-        [Property(DisplayName = "T1 is not equal to T2")]
-        public void FalseT1NotT2(NonNull<string> a, int b)
+        [Property(DisplayName = "First is not equal to Second")]
+        public void FirstNotEqualToSecond(NonNull<string> a, int b)
         {
             First<string, int>(a.Get)
                 .Equals(Second<string, int>(b))
                 .Run(result => Assert.False(result));
         }
 
-        [Property(DisplayName = "T2 is not equal to T1")]
-        public void FalseT2NotT1(NonNull<string> a, int b)
+        [Property(DisplayName = "Second is not equal to First")]
+        public void SecondNotEqualToFirst(NonNull<string> a, int b)
         {
             Second<string, int>(b)
                 .Equals(First<string, int>(a.Get))
                 .Run(result => Assert.False(result));
         }
 
-        [Property(DisplayName = "T1 are not the same")]
-        public void FalseT1(NonNull<string> a)
+        [Property(DisplayName = "First are not the same")]
+        public void FirstAreNotTheSame(NonNull<string> a)
         {
             First<string, int>($"a{a.Get}")
                 .Equals(First<string, int>($"b{a.Get}"))
                 .Run(result => Assert.False(result));
         }
 
-        [Property(DisplayName = "T2 are not the same")]
-        public void FalseT2(NonNull<string> a)
+        [Property(DisplayName = "Second are not the same")]
+        public void SecondAreNotTheSame(NonNull<string> a)
         {
             Second<int, string>($"a{a.Get}")
                 .Equals(Second<int, string>($"b{a.Get}"))
                 .Run(result => Assert.False(result));
         }
 
-        [Property(DisplayName = "T1 are equal")]
-        public void TrueT1(NonNull<string> a)
+        [Property(DisplayName = "First are equal")]
+        public void FirstEqual(NonNull<string> a)
         {
             First<string, int>(a.Get)
                 .Equals(First<string, int>(a.Get))
                 .Run(result => Assert.True(result));
         }
 
-        [Property(DisplayName = "T2 are equal")]
-        public void TrueT2(int a)
+        [Property(DisplayName = "Second are equal")]
+        public void SecondEqual(int a)
         {
             Second<string, int>(a)
                 .Equals(Second<string, int>(a))
