9efcaa5e2fe29ff44e13cc7c2246cdfe485beb72
JYCabello <j.cabello.ramos@gmail.com>
2021-09-20 14:11:12 +0200

    Coverate

---
+++ src/DeFuncto.Tests/Core/Extensions/Functions/AsyncFunction.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using DeFuncto.Extensions;
+using FsCheck;
+using FsCheck.Xunit;
+using Xunit;
+
+namespace DeFuncto.Tests.Core.Extensions.Functions
+{
+    public class AsyncFunction
+    {
+        [Property(DisplayName = "Creates an async function that does the same")]
+        public void Works(NonNull<string> val)
+        {
+            var witness = "unaware";
+            Func<Task> touch = () =>
+            {
+                witness = "aware";
+                return Task.CompletedTask;
+            };
+            Thread.Sleep(3);
+            Assert.Equal("unaware", witness);
+            var touchFunction = touch.AsyncFunction();
+            Thread.Sleep(3);
+            Assert.Equal("unaware", witness);
+            touchFunction().Wait();
+            Assert.Equal("aware", witness);
+        }
+    }
+}
