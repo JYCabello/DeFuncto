2f758ced26fcb12575f49f6b47bef8e9c92db419
JYCabello <j.cabello.ramos@gmail.com>
2021-09-20 14:15:14 +0200

    More coverage

--- src/DeFuncto.Tests/Core/Extensions/Functions/Function.cs
+++ src/DeFuncto.Tests/Core/Extensions/Functions/Function.cs
@@ -1,6 +1,4 @@
 ﻿using System;
-using System.Threading;
-using System.Threading.Tasks;
 using DeFuncto.Extensions;
 using FsCheck;
 using FsCheck.Xunit;
@@ -8,23 +6,20 @@ using Xunit;
 
 namespace DeFuncto.Tests.Core.Extensions.Functions
 {
-    public class AsyncFunction
+    public class Function
     {
-        [Property(DisplayName = "Creates an async function that does the same")]
+        [Property(DisplayName = "Creates a function that does the same")]
         public void Works(NonNull<string> a, NonNull<string> b)
         {
             var witness = a.Get;
-            Func<Task> touch = () =>
+            Action touch = () =>
             {
                 witness = b.Get;
-                return Task.CompletedTask;
             };
-            Thread.Sleep(1);
             Assert.Equal(a.Get, witness);
-            var touchFunction = touch.AsyncFunction();
-            Thread.Sleep(1);
+            var touchFunction = touch.Function();
             Assert.Equal(a.Get, witness);
-            touchFunction().Wait();
+            touchFunction();
             Assert.Equal(b.Get, witness);
         }
     }
