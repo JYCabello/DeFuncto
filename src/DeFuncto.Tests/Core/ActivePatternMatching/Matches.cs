be8cb249c425daffcc4d9d44ba84ec5737e3174c
JYCabello <j.cabello.ramos@gmail.com>
2021-09-20 14:55:53 +0200

    Version bump

--- src/DeFuncto.Tests/Core/ActivePatternMatching/Matches.cs
+++ src/DeFuncto.Tests/Core/ActivePatternMatching/Matches.cs
@@ -1,24 +1,63 @@
-﻿using Xunit;
+﻿using System.Collections;
+using System.Collections.Generic;
+using DeFuncto.ActivePatternMatching;
+using Xunit;
 using static DeFuncto.Prelude;
 
 namespace DeFuncto.Tests.Core.ActivePatternMatching
 {
     public class Matches
     {
-        [Fact(DisplayName = "Matches with the correct pattern")]
-        public void Works()
+        [Theory(DisplayName = "Matches with the correct pattern")]
+        [MemberData(nameof(Functions))]
+        public void Works(ActivePatternBase<string, string>[] patterns)
         {
-            var result = "correct"
-                .ActMatch(
-                    With(
-                        Pattern<string, string, string>(_ => None, _ => "incorrect"),
-                        Pattern<string, string, string>(Some, Id),
-                        Pattern<string, string, string>(_ => None, _ => "incorrect"),
-                        Pattern<string, string, string>(_ => None, _ => "incorrect")
-                    ),
-                    _ => "incorrect");
+            var result = "correct".ActMatch(patterns, _ => "incorrect");
             Assert.NotEqual("incorrect", result);
             Assert.Equal("correct", result);
+        }
+
+        public static IEnumerable<object[]> Functions()
+        {
+            yield return new object[]
+            {
+                With(
+                    Pattern<string, string, string>(Some, Id),
+                    Pattern<string, string, string>(_ => None, _ => "incorrect"),
+                    Pattern<string, string, string>(_ => None, _ => "incorrect"),
+                    Pattern<string, string, string>(_ => None, _ => "incorrect")
+                )
+            };
+
+            yield return new object[]
+            {
+                With(
+                    Pattern<string, string, string>(_ => None, _ => "incorrect"),
+                    Pattern<string, string, string>(Some, Id),
+                    Pattern<string, string, string>(_ => None, _ => "incorrect"),
+                    Pattern<string, string, string>(_ => None, _ => "incorrect")
+                )
+            };
+
+            yield return new object[]
+            {
+                With(
+                    Pattern<string, string, string>(_ => None, _ => "incorrect"),
+                    Pattern<string, string, string>(_ => None, _ => "incorrect"),
+                    Pattern<string, string, string>(Some, Id),
+                    Pattern<string, string, string>(_ => None, _ => "incorrect")
+                )
+            };
+
+            yield return new object[]
+            {
+                With(
+                    Pattern<string, string, string>(_ => None, _ => "incorrect"),
+                    Pattern<string, string, string>(_ => None, _ => "incorrect"),
+                    Pattern<string, string, string>(_ => None, _ => "incorrect"),
+                    Pattern<string, string, string>(Some, Id)
+                )
+            };
         }
     }
 }
