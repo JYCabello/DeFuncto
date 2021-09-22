f2aafc00c8c08170943bb9fbbeabda500bd31d03
R. van der Heijden <rheijdendeveloper@gmail.com>
2021-09-22 10:16:49 +0200

    Update Equals.cs

--- src/DeFuncto.Tests/Core/Types/Du/Equals.cs
+++ src/DeFuncto.Tests/Core/Types/Du/Equals.cs
@@ -11,56 +11,48 @@ namespace DeFuncto.Tests.Core.Types.Du
         [Property(DisplayName = "Is not equal to null")]
         public void FalseByNull(NonNull<string> a)
         {
-            First<string, int>(a.Get)
+            First<string, string>(a.Get)
                 .Equals((object)null)
                 .Run(result => Assert.False(result));
         }
 
+        [Property(DisplayName = "First is not equal to First")]
+        public void FirstNotEqualToFirst(NonNull<string> a)
+        {
+            First<string, string>($"a{a.Get}")
+                .Equals(First<string, string>($"b{a.Get}"))
+                .Run(result => Assert.False(result));
+        }
+
         [Property(DisplayName = "First is not equal to Second")]
-        public void FirstNotEqualToSecond(NonNull<string> a, int b)
+        public void FirstNotEqualToSecond(NonNull<string> a, NonNull<string> b)
         {
-            First<string, int>(a.Get)
-                .Equals(Second<string, int>(b))
+            First<string, string>(a.Get)
+                .Equals(Second<string, string>(b.Get))
                 .Run(result => Assert.False(result));
         }
 
-        [Property(DisplayName = "Second is not equal to First")]
-        public void SecondNotEqualToFirst(NonNull<string> a, int b)
+        [Property(DisplayName = "Second is not equal to Second")]
+        public void SecondNotEqualToSecond(NonNull<string> a)
         {
-            Second<string, int>(b)
-                .Equals(First<string, int>(a.Get))
+            Second<string, string>($"a{a.Get}")
+                .Equals(Second<string, string>($"b{a.Get}"))
                 .Run(result => Assert.False(result));
         }
 
-        [Property(DisplayName = "First are not the same")]
-        public void FirstAreNotTheSame(NonNull<string> a)
+        [Property(DisplayName = "First is equal to First")]
+        public void FirstEqualToFirst(NonNull<string> a)
         {
-            First<string, int>($"a{a.Get}")
-                .Equals(First<string, int>($"b{a.Get}"))
-                .Run(result => Assert.False(result));
-        }
-
-        [Property(DisplayName = "Second are not the same")]
-        public void SecondAreNotTheSame(NonNull<string> a)
-        {
-            Second<int, string>($"a{a.Get}")
-                .Equals(Second<int, string>($"b{a.Get}"))
-                .Run(result => Assert.False(result));
-        }
-
-        [Property(DisplayName = "First are equal")]
-        public void FirstEqual(NonNull<string> a)
-        {
-            First<string, int>(a.Get)
-                .Equals(First<string, int>(a.Get))
+            First<string, string>(a.Get)
+                .Equals(First<string, string>(a.Get))
                 .Run(result => Assert.True(result));
         }
 
-        [Property(DisplayName = "Second are equal")]
-        public void SecondEqual(int a)
+        [Property(DisplayName = "Second is equal to Second")]
+        public void SecondEqualToSecond(NonNull<string> a)
         {
-            Second<string, int>(a)
-                .Equals(Second<string, int>(a))
+            Second<string, string>(a.Get)
+                .Equals(Second<string, string>(a.Get))
                 .Run(result => Assert.True(result));
         }
     }
