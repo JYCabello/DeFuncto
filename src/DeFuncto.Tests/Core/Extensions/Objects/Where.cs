fed3931f019f725a605b4a2db31727f04fa7119d
R. van der Heijden <rheijdendeveloper@gmail.com>
2021-09-23 09:40:54 +0200

    Where extension for Object

---
+++ src/DeFuncto.Tests/Core/Extensions/Objects/Where.cs
@@ -0,0 +1,23 @@
+﻿using FsCheck;
+using FsCheck.Xunit;
+using System.Linq;
+using DeFuncto.Extensions;
+using DeFuncto.Assertions;
+
+namespace DeFuncto.Tests.Core.Extensions.Objects
+{
+    public class Where
+    {
+        [Property]
+        public void Some(NonNull<string> a)
+        {
+            a.Where(x => x == a).ShouldBeSome(a);
+        }
+
+        [Property]
+        public void None(NonNull<string> a)
+        {
+            a.Where(x => x != a).ShouldBeNone();
+        }
+    }
+}
