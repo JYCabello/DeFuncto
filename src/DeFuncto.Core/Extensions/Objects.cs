d7772675a72074f1400723ac4ee4ad15829c4a76
JYCabello <j.cabello.ramos@gmail.com>
2021-09-23 16:11:21 +0200

    Renamed where to when

--- src/DeFuncto.Core/Extensions/Objects.cs
+++ src/DeFuncto.Core/Extensions/Objects.cs
@@ -34,7 +34,7 @@ namespace DeFuncto.Extensions
 
         [Pure]
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public static Option<T> Where<T>(this T self, Func<T, bool> predicate) =>
+        public static Option<T> When<T>(this T self, Func<T, bool> predicate) =>
             predicate(self) ? Some(self) : None;
     }
 }
