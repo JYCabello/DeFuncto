f2806821a15bb79d42e029cfdc66ab1faa6e52c9
Rob van der Heijden <rheijdendeveloper@gmail.com>
2021-11-05 09:28:21 +0100

    Match for booleans

---
+++ src/DeFuncto.Tests/Core/Booleans/Match.cs
@@ -0,0 +1,51 @@
+﻿using System.Threading.Tasks;
+using DeFuncto.Extensions;
+using Xunit;
+
+namespace DeFuncto.Tests.Core.Booleans
+{
+    public class Match
+    {
+        [Fact]
+        public void PositiveMatchFunction()
+        {
+            string result = true.Match(() => "A", () => "B");
+            Assert.Equal("A", result);
+        }
+
+        [Fact]
+        public void NegativeMatchFunction()
+        {
+            string result = false.Match(() => "A", () => "B");
+            Assert.Equal("B", result);
+        }
+
+        [Fact]
+        public async Task PositiveTaskMatchFunction()
+        {
+            string result = await true.ToTask().Match(() => "A", () => "B");
+            Assert.Equal("A", result);
+        }
+
+        [Fact]
+        public async Task NegativeTaskMatchFunction()
+        {
+            string result = await false.ToTask().Match(() => "A", () => "B");
+            Assert.Equal("B", result);
+        }
+
+        [Fact]
+        public async Task PositiveTaskMatchFunctionTask()
+        {
+            string result = await true.ToTask().Match(() => "A".ToTask(), () => "B".ToTask());
+            Assert.Equal("A", result);
+        }
+
+        [Fact]
+        public async Task NegativeTaskMatchFunctionTask()
+        {
+            string result = await false.ToTask().Match(() => "A".ToTask(), () => "B".ToTask());
+            Assert.Equal("B", result);
+        }
+    }
+}
