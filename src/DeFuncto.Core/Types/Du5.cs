d926a18485d796870f0543532f020ab3d56c47bc
Yeray Cabello <j.cabello.ramos@gmail.com>
2022-02-09 09:11:35 +0100

    cleanup

--- src/DeFuncto.Core/Types/Du5.cs
+++ src/DeFuncto.Core/Types/Du5.cs
@@ -132,66 +132,47 @@ namespace DeFuncto
             obj is Du5<T1, T2, T3, T4, T5> other && Equals(other);
 
         public bool Equals(Du5<T1, T2, T3, T4, T5> other) =>
-             Discriminator == other.Discriminator
-             && Match(
-                 v => v!.Equals(other.t1),
-                 v => v!.Equals(other.t2),
-                 v => v!.Equals(other.t3),
-                 v => v!.Equals(other.t4),
-                 v => v!.Equals(other.t5));
+            Discriminator == other.Discriminator
+            && Match(
+                v => v!.Equals(other.t1),
+                v => v!.Equals(other.t2),
+                v => v!.Equals(other.t3),
+                v => v!.Equals(other.t4),
+                v => v!.Equals(other.t5));
 
         public override int GetHashCode() =>
             (this, -815974257)
-                .Apply(t => (t.Item1, t.Item2 * -1521134295 + EqualityComparer<T1?>.Default.GetHashCode(t.Item1.t1)))
-                .Apply(t => (t.Item1, t.Item2 * -1521134295 + EqualityComparer<T2?>.Default.GetHashCode(t.Item1.t2)))
-                .Apply(t => (t.Item1, t.Item2 * -1521134295 + EqualityComparer<T3?>.Default.GetHashCode(t.Item1.t3)))
-                .Apply(t => (t.Item1, t.Item2 * -1521134295 + EqualityComparer<T4?>.Default.GetHashCode(t.Item1.t4)))
-                .Apply(t => (t.Item1, t.Item2 * -1521134295 + EqualityComparer<T5?>.Default.GetHashCode(t.Item1.t5)))
-                .Apply(t => t.Item2 * -1521134295 + t.Item1.Discriminator.GetHashCode());
+            .Apply(t => (t.Item1, t.Item2 * -1521134295 + EqualityComparer<T1?>.Default.GetHashCode(t.Item1.t1)))
+            .Apply(t => (t.Item1, t.Item2 * -1521134295 + EqualityComparer<T2?>.Default.GetHashCode(t.Item1.t2)))
+            .Apply(t => (t.Item1, t.Item2 * -1521134295 + EqualityComparer<T3?>.Default.GetHashCode(t.Item1.t3)))
+            .Apply(t => (t.Item1, t.Item2 * -1521134295 + EqualityComparer<T4?>.Default.GetHashCode(t.Item1.t4)))
+            .Apply(t => (t.Item1, t.Item2 * -1521134295 + EqualityComparer<T5?>.Default.GetHashCode(t.Item1.t5)))
+            .Apply(t => t.Item2 * -1521134295 + t.Item1.Discriminator.GetHashCode());
 
-        [Pure]
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public Unit Iter(Action<T1> ont1, Action<T2> ont2, Action<T3> ont3, Action<T4> ont4, Action<T5> ont5) =>
-            Match(
-                t1 => { ont1(t1); return unit; },
-                t2 => { ont2(t2); return unit; },
-                t3 => { ont3(t3); return unit; },
-                t4 => { ont4(t4); return unit; },
-                t5 => { ont5(t5); return unit; }
-            );
+            Iter(ont1.Function(), ont2.Function(), ont3.Function(), ont4.Function(), ont5.Function());
 
-        [Pure]
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public Unit Iter(Func<T1, Unit> ont1, Func<T2, Unit> ont2, Func<T3, Unit> ont3, Func<T4, Unit> ont4, Func<T5, Unit> ont5) =>
-            Iter(
-                t1 => { ont1(t1); },
-                t2 => { ont2(t2); },
-                t3 => { ont3(t3); },
-                t4 => { ont4(t4); },
-                t5 => { ont5(t5); }
-            );
+            Match(ont1, ont2, ont3, ont4, ont5);
 
-        [Pure]
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public Unit Iter(Func<T1, Unit> ont1) =>
-             Iter(ont1, (_) => unit, (_) => unit, (_) => unit, (_) => unit);
+            Iter(ont1, (_) => unit, (_) => unit, (_) => unit, (_) => unit);
 
-        [Pure]
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public Unit Iter(Func<T2, Unit> ont2) =>
             Iter((_) => unit, ont2, (_) => unit, (_) => unit, (_) => unit);
 
-        [Pure]
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public Unit Iter(Func<T3, Unit> ont3) =>
             Iter((_) => unit, (_) => unit, ont3, (_) => unit, (_) => unit);
 
-        [Pure]
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public Unit Iter(Func<T4, Unit> ont4) =>
             Iter((_) => unit, (_) => unit, (_) => unit, ont4, (_) => unit);
 
-        [Pure]
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public Unit Iter(Func<T5, Unit> ont5) =>
             Iter((_) => unit, (_) => unit, (_) => unit, (_) => unit, ont5);
