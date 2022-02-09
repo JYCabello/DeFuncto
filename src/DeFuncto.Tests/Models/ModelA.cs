b239690b7935faace1dda53f2bb301166ea2eab8
Yeray Cabello <j.cabello.ramos@gmail.com>
2022-02-09 09:16:00 +0100

    du4 done

--- src/DeFuncto.Tests/Models/ModelA.cs
+++ src/DeFuncto.Tests/Models/ModelA.cs
@@ -1,24 +1,13 @@
-﻿namespace DeFuncto.Tests.Models
-{
-    internal struct Model1
-    {
-    }
-    internal struct Model2
-    {
-    }
-    internal struct Model3
-    {
-    }
+﻿namespace DeFuncto.Tests.Models;
 
-    internal struct Model4
-    {
-    }
+internal struct Model1 { }
 
-    internal struct Model5
-    {
-    }
+internal struct Model2 { }
 
-    internal struct Model6
-    {
-    }
-}
+internal struct Model3 { }
+
+internal struct Model4 { }
+
+internal struct Model5 { }
+
+internal struct Model6 { }
