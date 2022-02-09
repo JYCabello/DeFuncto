545f9875fc9c17e03ff4f8420e5b3e3d8105833e
Yeray Cabello <j.cabello.ramos@gmail.com>
2022-02-09 09:50:45 +0100

    Updated pipelines

--- src/DeFuncto.Assertions/WitnessAssertions.cs
+++ src/DeFuncto.Assertions/WitnessAssertions.cs
@@ -1,18 +1,17 @@
-﻿namespace DeFuncto.Assertions
+﻿namespace DeFuncto.Assertions;
+
+public static class WitnessAssertions
 {
-    public static class WitnessAssertions
+    public static void ShouldHaveBeenTouched(this Witness self)
     {
-        public static void ShouldHaveBeenTouched(this Witness self)
-        {
-            if (self.TimesCalled < 1)
-                throw new AssertionFailed("Witness was not called");
-        }
+        if (self.TimesCalled < 1)
+            throw new AssertionFailed("Witness was not called");
+    }
 
 
-        public static void ShouldHaveBeenTouched(this Witness self, int times)
-        {
-            if (self.TimesCalled != times)
-                throw new AssertionFailed($"Witness should have been called {times} times but was {self.TimesCalled}");
-        }
+    public static void ShouldHaveBeenTouched(this Witness self, int times)
+    {
+        if (self.TimesCalled != times)
+            throw new AssertionFailed($"Witness should have been called {times} times but was {self.TimesCalled}");
     }
 }
