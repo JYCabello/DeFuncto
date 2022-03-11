d11a2ce8b3532b890f700f9471fc8a2fb2d061ac
Yeray Cabello <j.cabello.ramos@gmail.com>
2022-03-11 12:28:11 +0100

    Mapping nullable

---
+++ src/DeFuncto.Core/Extensions/Nullables.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace DeFuncto.Extensions;
+
+public static class Nullables
+{
+    public static TReturn? Map<T, TReturn>(this T? self, Func<T, TReturn> f)
+        where T : struct
+        where TReturn : struct =>
+        self is null
+            ? null
+            : f(self.Value);
+}
