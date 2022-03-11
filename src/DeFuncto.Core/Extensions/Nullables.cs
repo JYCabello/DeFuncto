c0b7eecb6ff896cc3dc98d3aeb46d9e56ff3674d
Yeray Cabello <j.cabello.ramos@gmail.com>
2022-03-11 12:35:56 +0100

    Binding nullables

--- src/DeFuncto.Core/Extensions/Nullables.cs
+++ src/DeFuncto.Core/Extensions/Nullables.cs
@@ -10,4 +10,11 @@ public static class Nullables
         self is null
             ? null
             : f(self.Value);
+
+    public static TReturn? Bind<T, TReturn>(this T? self, Func<T, TReturn?> f)
+        where T : struct
+        where TReturn : struct =>
+        self is null
+            ? null
+            : f(self.Value);
 }
---
+++ src/DeFuncto.Tests/Core/Extensions/Nullables/Bind.cs
@@ -0,0 +1,23 @@
+﻿using DeFuncto.Extensions;
+using FsCheck.Xunit;
+using Xunit;
+
+namespace DeFuncto.Tests.Core.Extensions.Nullables;
+
+public class Bind
+{
+    [Property(DisplayName = "Binds the value and skips null")]
+    public void Maps(int? value, int? anotherValue)
+    {
+        var result = value.Bind(v => anotherValue.Map(av => av + v));
+
+        if (value is not null && anotherValue is not null)
+        {
+            Assert.NotNull(result);
+            Assert.Equal(value.Value + anotherValue.Value, result.Value);
+        }
+
+        if (value is null || anotherValue is null)
+            Assert.Null(result);
+    }
+}
