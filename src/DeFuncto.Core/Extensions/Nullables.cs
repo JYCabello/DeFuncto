bd2ecb6714f030aa0c6d1d1396d59d9b1c69b68a
Yeray Cabello <j.cabello.ramos@gmail.com>
2022-03-11 15:11:06 +0100

    Map nullables (#82)

---
+++ src/DeFuncto.Core/Extensions/Nullables.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace DeFuncto.Extensions;
+
+public static class Nullables
+{
+    public static TReturn? Map<T, TReturn>(this T? self, Func<T, TReturn> f)
+        where T : struct
+        where TReturn : struct =>
+        self is null ? null : f(self.Value);
+
+    public static TReturn? Bind<T, TReturn>(this T? self, Func<T, TReturn?> f)
+        where T : struct
+        where TReturn : struct =>
+        self is null ? null : f(self.Value);
+}
---
+++ src/DeFuncto.Tests/Core/Extensions/Nullables/Map.cs
@@ -0,0 +1,24 @@
+﻿using DeFuncto.Extensions;
+using FsCheck.Xunit;
+using Xunit;
+
+namespace DeFuncto.Tests.Core.Extensions.Nullables;
+
+public class Map
+{
+    [Property(DisplayName = "Maps the value")]
+    public void Maps(int value)
+    {
+        int? nullableValue = value;
+        var result = nullableValue.Map(v => v + 1);
+        Assert.NotNull(result);
+        Assert.Equal(value + 1, result.Value);
+    }
+
+    [Fact(DisplayName = "Skips null")]
+    public void Skips()
+    {
+        var result = ((int?) null).Map(v => v + 1);
+        Assert.Null(result);
+    }
+}
