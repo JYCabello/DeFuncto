12f3aadb056b3ff684d583900d7bebf1e7514804
Yeray Cabello <j.cabello.ramos@gmail.com>
2022-03-11 12:55:52 +0100

    Formatting

--- src/DeFuncto.Core/Extensions/Nullables.cs
+++ src/DeFuncto.Core/Extensions/Nullables.cs
@@ -7,14 +7,10 @@ public static class Nullables
     public static TReturn? Map<T, TReturn>(this T? self, Func<T, TReturn> f)
         where T : struct
         where TReturn : struct =>
-        self is null
-            ? null
-            : f(self.Value);
+        self is null ? null : f(self.Value);
 
     public static TReturn? Bind<T, TReturn>(this T? self, Func<T, TReturn?> f)
         where T : struct
         where TReturn : struct =>
-        self is null
-            ? null
-            : f(self.Value);
+        self is null ? null : f(self.Value);
 }
