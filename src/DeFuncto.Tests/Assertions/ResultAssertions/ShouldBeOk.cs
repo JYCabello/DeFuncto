d2adb787ab8a741755b3bfb52d52ae641712efab
Yeray Cabello <j.cabello.ramos@gmail.com>
2022-03-14 20:45:06 +0100

    some shouldbeOK

---
+++ src/DeFuncto.Tests/Assertions/ResultAssertions/ShouldBeOk.cs
@@ -0,0 +1,30 @@
+﻿using System.Threading.Tasks;
+using DeFuncto.Assertions;
+using FsCheck;
+using FsCheck.Xunit;
+using Xunit;
+using static DeFuncto.Prelude;
+
+namespace DeFuncto.Tests.Assertions.ResultAssertions;
+
+public class ShouldBeOk
+{
+    [Property(DisplayName = "Fails when it's not OK")]
+    public async Task FailsNotOk(NonNull<string> a)
+    {
+        var error = Error<int, string>(a.Get).Async();
+        await Assert.ThrowsAsync<AssertionFailedException>(() => error.ShouldBeOk());
+    }
+
+    [Property(DisplayName = "Fails when it's not the same value for OK")]
+    public void FailsNotSameOk(NonNull<string> a, NonNull<string> b)
+    {
+        var ok = Ok<string, int>(a.Get).Async();
+        _ = ok.ShouldBeOk(a.Get).Result;
+        if (a.Get != b.Get)
+            _ = Assert.ThrowsAsync<AssertionFailedException>(async () =>
+            {
+                await ok.ShouldBeOk(b.Get);
+            }).Result;
+    }
+}
