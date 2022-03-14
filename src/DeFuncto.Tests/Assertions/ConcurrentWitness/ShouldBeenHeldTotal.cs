941226e55cd2a639d5e7fed359e76984d47fe154
Yeray Cabello <j.cabello.ramos@gmail.com>
2022-03-14 20:01:30 +0100

    Testing total concurrent witness holds

---
+++ src/DeFuncto.Tests/Assertions/ConcurrentWitness/ShouldBeenHeldTotal.cs
@@ -0,0 +1,17 @@
+﻿using DeFuncto.Assertions;
+using Xunit;
+
+namespace DeFuncto.Tests.Assertions.ConcurrentWitness;
+
+public class ShouldBeenHeldTotal
+{
+    [Fact(DisplayName = "Fails when it has been held a different amount of times")]
+    public void Fails()
+    {
+        var witness = new DeFuncto.Assertions.ConcurrentWitness();
+        using (var _ = witness.Grab()) { }
+        witness.ShouldBeenHeldTotal(1);
+        Assert.Throws<AssertionFailedException>(() => witness.ShouldBeenHeldTotal(2));
+        Assert.Throws<AssertionFailedException>(() => witness.ShouldBeenHeldTotal(0));
+    }
+}
