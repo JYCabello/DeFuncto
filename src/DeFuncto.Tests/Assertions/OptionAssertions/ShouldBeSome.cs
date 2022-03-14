e402f975ee63c0259123c46c78b5d8a36b67ed0c
Yeray Cabello <j.cabello.ramos@gmail.com>
2022-03-14 17:00:46 +0100

    covering option assertions

---
+++ src/DeFuncto.Tests/Assertions/OptionAssertions/ShouldBeSome.cs
@@ -0,0 +1,30 @@
+﻿using System.Threading.Tasks;
+using DeFuncto.Assertions;
+using Xunit;
+using static DeFuncto.Prelude;
+
+namespace DeFuncto.Tests.Assertions.OptionAssertions;
+
+public class ShouldBeSome
+{
+    [Fact(DisplayName = "Should fail if is none")]
+    public void FailNone()
+    {
+        Option<int> none = None;
+        Assert.Throws<AssertionFailedException>(() => none.ShouldBeSome());
+    }
+
+    [Fact(DisplayName = "Should fail if is different some")]
+    public void FailDifferentNone()
+    {
+        var some = Some(1);
+        Assert.Throws<AssertionFailedException>(() => some.ShouldBeSome(2));
+    }
+
+    [Fact(DisplayName = "Should fail if is different some async")]
+    public async Task FailDifferentNoneAsyncc()
+    {
+        var some = Some(1).Async();
+        await Assert.ThrowsAsync<AssertionFailedException>(() => some.ShouldBeSome(2));
+    }
+}
