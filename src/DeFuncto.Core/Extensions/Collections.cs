4067d777071337d552564aac8108d90631a2f196
Yeray Cabello <j.cabello.ramos@gmail.com>
2022-03-29 10:03:36 +0200

    moar docs

--- src/DeFuncto.Core/Extensions/Collections.cs
+++ src/DeFuncto.Core/Extensions/Collections.cs
@@ -15,22 +15,36 @@ namespace DeFuncto.Extensions;
 public static class Collections
 {
     /// <summary>
-    /// Out of an Enumerable of Options, filter and map it to an Enumerable
-    /// with the values of the Options in the Some state.
+    /// Taking an Enumerable of type Option T, get only the present values in a new Enumerable of type T.
     /// </summary>
     /// <param name="self">An enumerable of Options.</param>
-    /// <typeparam name="T">Type of the value of the Options.</typeparam>
+    /// <typeparam name="T">Value type of the Options.</typeparam>
     /// <returns>An Enumerable with the values of the Options in the Some state.</returns>
     [Pure]
     [MethodImpl(MethodImplOptions.AggressiveInlining)]
     public static IEnumerable<T> Choose<T>(this IEnumerable<Option<T>> self) =>
         self.Where(opt => opt.IsSome).Select(opt => opt.Match(Id, () => default!));
 
+    /// <summary>
+    /// Taking an Enumerable of type T, apply a function to every element to an Option of type T,
+    /// then get only the present values in a new Enumerable of type T.
+    /// </summary>
+    /// <param name="self">The Enumerable to process.</param>
+    /// <param name="f">Projection function.</param>
+    /// <typeparam name="T">Input type.</typeparam>
+    /// <typeparam name="TOut">Value type of the Option in the projection.</typeparam>
+    /// <returns>An Enumerable with the values of the Options with a present value.</returns>
     [Pure]
     [MethodImpl(MethodImplOptions.AggressiveInlining)]
     public static IEnumerable<TOut> Choose<T, TOut>(this IEnumerable<T> self, Func<T, Option<TOut>> f) =>
         self.Select(f).Choose();
 
+    /// <summary>
+    /// Gets the first element of an Enumerable as Some, or None if there was no value.
+    /// </summary>
+    /// <param name="self">The Enumerable to get the first element from.</param>
+    /// <typeparam name="T">Value type.</typeparam>
+    /// <returns></returns>
     [Pure]
     [MethodImpl(MethodImplOptions.AggressiveInlining)]
     public static Option<T> FirstOrNone<T>(this IEnumerable<T> self) =>
