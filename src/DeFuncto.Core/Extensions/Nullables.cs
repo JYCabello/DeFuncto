bca2b2c0a7b3f7728ef22c296932acbdac4ee83a
Yeray Cabello <j.cabello.ramos@gmail.com>
2022-05-03 12:55:19 +0200

    nullables

--- src/DeFuncto.Core/Extensions/Nullables.cs
+++ src/DeFuncto.Core/Extensions/Nullables.cs
@@ -2,13 +2,32 @@
 
 namespace DeFuncto.Extensions;
 
+/// <summary>
+/// Functions to compose nullables as if they where options.
+/// </summary>
 public static class Nullables
 {
+    /// <summary>
+    /// Maps a nullable value.
+    /// </summary>
+    /// <param name="self">The nullable to map.</param>
+    /// <param name="f">The projection.</param>
+    /// <typeparam name="T">Input type.</typeparam>
+    /// <typeparam name="TReturn">Projected type.</typeparam>
+    /// <returns>Projected value, or null.</returns>
     public static TReturn? Map<T, TReturn>(this T? self, Func<T, TReturn> f)
         where T : struct
         where TReturn : struct =>
         self is null ? null : f(self.Value);
 
+    /// <summary>
+    /// Binds a nullable value.
+    /// </summary>
+    /// <param name="self">The nullable to map.</param>
+    /// <param name="f">The projection to a nullable.</param>
+    /// <typeparam name="T">Input type.</typeparam>
+    /// <typeparam name="TReturn">Projected type.</typeparam>
+    /// <returns>Projected value, or null.</returns>
     public static TReturn? Bind<T, TReturn>(this T? self, Func<T, TReturn?> f)
         where T : struct
         where TReturn : struct =>
