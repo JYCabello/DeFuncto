8969bd0365ba8f9a5d73f4c7c6e005e50d3d82aa
Yeray Cabello <j.cabello.ramos@gmail.com>
2022-05-04 11:04:42 +0200

    More docs

--- src/DeFuncto.Core/Extensions/Objects.cs
+++ src/DeFuncto.Core/Extensions/Objects.cs
@@ -6,8 +6,19 @@ using static DeFuncto.Prelude;
 
 namespace DeFuncto.Extensions;
 
+/// <summary>
+/// Extension methods to apply functions to any kind of object.
+/// </summary>
 public static class Objects
 {
+    /// <summary>
+    /// Apply a function to an object.
+    /// </summary>
+    /// <param name="self">The object to apply the function to.</param>
+    /// <param name="f">The function to apply.</param>
+    /// <typeparam name="TIn">Type of the object we'll apply the function to.</typeparam>
+    /// <typeparam name="TOut">Output type of the function.</typeparam>
+    /// <returns>The output of the applied function.</returns>
     [Pure]
     [MethodImpl(MethodImplOptions.AggressiveInlining)]
     public static TOut Apply<TIn, TOut>(this TIn self, Func<TIn, TOut> f) => f(self);
