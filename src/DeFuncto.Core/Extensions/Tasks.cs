fbc5f985ac46ae887d07efcaedb76070f04b8783
Yeray Cabello <j.cabello.ramos@gmail.com>
2022-05-13 12:40:06 +0200

    more comments

--- src/DeFuncto.Core/Extensions/Tasks.cs
+++ src/DeFuncto.Core/Extensions/Tasks.cs
@@ -8,24 +8,45 @@ using System.Threading.Tasks;
 
 namespace DeFuncto.Extensions;
 
+/// <summary>
+/// Functions to work with "what's inside a task".
+/// </summary>
 public static class Tasks
 {
-    [Pure]
+    /// <summary>
+    /// Maps the return value of a task with a synchronous function.
+    /// </summary>
+    /// <param name="self">Task to map.</param>
+    /// <param name="f">Projection function.</param>
+    /// <typeparam name="TIn">Return type in the original task.</typeparam>
+    /// <typeparam name="TOut">The type of the resulting task.</typeparam>
+    /// <returns>
+    /// A task that will return the result of computing the original task's result with the
+    /// projection function.
+    /// </returns>
     [MethodImpl(MethodImplOptions.AggressiveInlining)]
     public static async Task<TOut> Map<TIn, TOut>(this Task<TIn> self, Func<TIn, TOut> f) =>
         f(await self);
 
-    [Pure]
+    /// <summary>
+    /// Binds the output of a task to the result of an asynchronous function.
+    /// </summary>
+    /// <param name="self">Task to bind.</param>
+    /// <param name="f">Projection function returning a Task.</param>
+    /// <typeparam name="TIn">Return type in the original task.</typeparam>
+    /// <typeparam name="TOut">The type of the resulting task.</typeparam>
+    /// <returns>
+    /// A task that will return the result of computing the original task's result with the
+    /// projection function and flattening with the wrapping task.
+    /// </returns>
     [MethodImpl(MethodImplOptions.AggressiveInlining)]
     public static async Task<TOut> Map<TIn, TOut>(this Task<TIn> self, Func<TIn, Task<TOut>> f) =>
         await f(await self);
 
-    [Pure]
     [MethodImpl(MethodImplOptions.AggressiveInlining)]
     public static async Task<T> Flatten<T>(this Task<Task<T>> self) =>
         await await self;
 
-    [Pure]
     [MethodImpl(MethodImplOptions.AggressiveInlining)]
     public static Task<List<T>> Flatten<T>(this Task<List<T>[]> self) =>
         self.Map(t => t.SelectMany(a => a)).ToList();
@@ -80,37 +101,30 @@ public static class Tasks
     public static Task<Unit> RunAsync<T>(this T self, Func<T, Task<Unit>> f) =>
         self.RunAsync(async t => { await f(t); });
 
-    [Pure]
     [MethodImpl(MethodImplOptions.AggressiveInlining)]
     public static Task<IEnumerable<TOut>> Select<TIn, TOut>(this Task<IEnumerable<TIn>> self, Func<TIn, TOut> func) =>
         self.Map(ienumerable => ienumerable.Select(func));
 
-    [Pure]
     [MethodImpl(MethodImplOptions.AggressiveInlining)]
     public static Task<IEnumerable<TOut>> Select<TIn, TOut>(this Task<List<TIn>> self, Func<TIn, TOut> func) =>
         self.Map(ienumerable => ienumerable.Select(func));
 
-    [Pure]
     [MethodImpl(MethodImplOptions.AggressiveInlining)]
     public static Task<IEnumerable<TResult>> Select<TIn, TResult>(this Task<TIn[]> self, Func<TIn, TResult> mapper) =>
         self.Map(x => x.Select(mapper));
 
-    [Pure]
     [MethodImpl(MethodImplOptions.AggressiveInlining)]
     public static Task<List<T>> ToList<T>(this Task<IEnumerable<T>> self) =>
         self.Map(t => t.ToList());
 
-    [Pure]
     [MethodImpl(MethodImplOptions.AggressiveInlining)]
     public static Task<List<T>> ToList<T>(this Task<T[]> self) =>
         self.Map(t => t.ToList());
 
-    [Pure]
     [MethodImpl(MethodImplOptions.AggressiveInlining)]
     public static Task<T[]> ToArray<T>(this Task<IEnumerable<T>> self) =>
         self.Map(t => t.ToArray());
 
-    [Pure]
     [MethodImpl(MethodImplOptions.AggressiveInlining)]
     public static Task<T[]> ToArray<T>(this Task<List<T>> self) =>
         self.Map(t => t.ToArray());
