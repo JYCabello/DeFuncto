52a91b0d28dbad3991af014666267de1efb247dd
Yeray Cabello <j.cabello.ramos@gmail.com>
2022-05-14 11:09:22 +0200

    comments

--- src/DeFuncto.Core/ActivePatternMatching/ActivePattern.cs
+++ src/DeFuncto.Core/ActivePatternMatching/ActivePattern.cs
@@ -81,11 +81,13 @@ public class ActivePattern<TIn, TIntermediate, TOut> : ActivePatternBase<TIn, TO
 
 /// <summary>
 /// Function to execute active pattern matching.
+/// </summary>
+/// <remarks>
 /// This bit is not, and never will, be documented on the official documentation.
 /// If you reach this point, this makes sense to you and you want to use it, it's time
 /// to make a move to F# or to step back in your usage of functional programming.
-/// You are on the edge of what C# can do for you when it comes to FP.
-/// </summary>
+/// You are on the edge of what I believe C# *should* do for you when it comes to FP.
+/// </remarks>
 internal static class ActivePatternsExecutor
 {
     /// <summary>
