4e35bb217c4cc5cd850d012ebad39e935766ce97
Yeray Cabello <j.cabello.ramos@gmail.com>
2022-05-16 10:38:21 +0200

    Tuples documented

--- src/DeFuncto.Core/Extensions/Tuples.cs
+++ src/DeFuncto.Core/Extensions/Tuples.cs
@@ -27,6 +27,17 @@ public static class Tuples
     ) =>
         f(self.Item1, self.Item2);
 
+    /// <summary>
+    /// Applies a function to the tuple, facilitating the deconstruction by
+    /// passing the elements of the tuple to the function.
+    /// </summary>
+    /// <param name="self">The tuple.</param>
+    /// <param name="f">Projection function.</param>
+    /// <typeparam name="T1">Type of the first element of the tuple.</typeparam>
+    /// <typeparam name="T2">Type of the second element of the tuple.</typeparam>
+    /// <typeparam name="T3">Type of the third element of the tuple.</typeparam>
+    /// <typeparam name="TOut">Output type of the projection.</typeparam>
+    /// <returns>The output of the projection.</returns>
     [Pure]
     [MethodImpl(MethodImplOptions.AggressiveInlining)]
     public static TOut Apply<T1, T2, T3, TOut>(
@@ -34,6 +45,19 @@ public static class Tuples
         Func<T1, T2, T3, TOut> f
     ) =>
         f(self.Item1, self.Item2, self.Item3);
+
+    /// <summary>
+    /// Applies a function to the tuple, facilitating the deconstruction by
+    /// passing the elements of the tuple to the function.
+    /// </summary>
+    /// <param name="self">The tuple.</param>
+    /// <param name="f">Projection function.</param>
+    /// <typeparam name="T1">Type of the first element of the tuple.</typeparam>
+    /// <typeparam name="T2">Type of the second element of the tuple.</typeparam>
+    /// <typeparam name="T3">Type of the third element of the tuple.</typeparam>
+    /// <typeparam name="T4">Type of the fourth element of the tuple.</typeparam>
+    /// <typeparam name="TOut">Output type of the projection.</typeparam>
+    /// <returns>The output of the projection.</returns>
 
     [Pure]
     [MethodImpl(MethodImplOptions.AggressiveInlining)]
@@ -43,6 +67,19 @@ public static class Tuples
     ) =>
         f(self.Item1, self.Item2, self.Item3, self.Item4);
 
+    /// <summary>
+    /// Applies a function to the tuple, facilitating the deconstruction by
+    /// passing the elements of the tuple to the function.
+    /// </summary>
+    /// <param name="self">The tuple.</param>
+    /// <param name="f">Projection function.</param>
+    /// <typeparam name="T1">Type of the first element of the tuple.</typeparam>
+    /// <typeparam name="T2">Type of the second element of the tuple.</typeparam>
+    /// <typeparam name="T3">Type of the third element of the tuple.</typeparam>
+    /// <typeparam name="T4">Type of the fourth element of the tuple.</typeparam>
+    /// <typeparam name="T5">Type of the fifth element of the tuple.</typeparam>
+    /// <typeparam name="TOut">Output type of the projection.</typeparam>
+    /// <returns>The output of the projection.</returns>
     [Pure]
     [MethodImpl(MethodImplOptions.AggressiveInlining)]
     public static TOut Apply<T1, T2, T3, T4, T5, TOut>(
@@ -51,6 +88,20 @@ public static class Tuples
     ) =>
         f(self.Item1, self.Item2, self.Item3, self.Item4, self.Item5);
 
+    /// <summary>
+    /// Applies a function to the tuple, facilitating the deconstruction by
+    /// passing the elements of the tuple to the function.
+    /// </summary>
+    /// <param name="self">The tuple.</param>
+    /// <param name="f">Projection function.</param>
+    /// <typeparam name="T1">Type of the first element of the tuple.</typeparam>
+    /// <typeparam name="T2">Type of the second element of the tuple.</typeparam>
+    /// <typeparam name="T3">Type of the third element of the tuple.</typeparam>
+    /// <typeparam name="T4">Type of the fourth element of the tuple.</typeparam>
+    /// <typeparam name="T5">Type of the fifth element of the tuple.</typeparam>
+    /// <typeparam name="T6">Type of the sixth element of the tuple.</typeparam>
+    /// <typeparam name="TOut">Output type of the projection.</typeparam>
+    /// <returns>The output of the projection.</returns>
     [Pure]
     [MethodImpl(MethodImplOptions.AggressiveInlining)]
     public static TOut Apply<T1, T2, T3, T4, T5, T6, TOut>(
