f591088ef9f6aad066376efb176fb3960da7c7fa
Yeray Cabello <j.cabello.ramos@gmail.com>
2022-07-27 16:38:49 +0200

    Awaitables (#99)

---
+++ src/DeFuncto.Tests/Core/Types/AsyncOption/GetAwaiter.cs
@@ -0,0 +1,27 @@
+﻿using System.Threading.Tasks;
+using DeFuncto.Assertions;
+using Xunit;
+using static DeFuncto.Prelude;
+
+namespace DeFuncto.Tests.Core.Types.AsyncOption;
+
+public class GetAwaiter
+{
+    [Fact(DisplayName = "Awaits some")]
+    public async Task AwaitsSome()
+    {
+        var opt = await Some(1).Async();
+        opt.ShouldBeSome(n =>
+        {
+            Assert.Equal(1, n);
+            return unit;
+        });
+    }
+
+    [Fact(DisplayName = "Awaits none")]
+    public async Task AwaitsNone()
+    {
+        var opt = await None.Option<int>().Async();
+        opt.ShouldBeNone();
+    }
+}
