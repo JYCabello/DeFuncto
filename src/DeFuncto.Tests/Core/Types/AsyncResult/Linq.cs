a181dbcfa9c406c0aeb75c98434066b0fb4694be
Aridane Alamo <aralmo@gmail.com>
2025-09-01 16:16:47 +0200

    appease the PR gods.

--- src/DeFuncto.Tests/Core/Types/AsyncResult/Linq.cs
+++ src/DeFuncto.Tests/Core/Types/AsyncResult/Linq.cs
@@ -52,49 +52,48 @@ public class Linq
     }
 
     [Property(DisplayName = "AsyncResult Select should project results")]
-    public async void SelectProjectsResult()
+    public void SelectProjectsResult()
     {
         // leave the type castings to ensure it's not returning a nested Result<Result...>>
 
-        await ((AsyncResult<decimal, int>)(from _ in Ok<string, int>(string.Empty).Async() select Ok<decimal, int>(decimal.Zero).Async())).ShouldBeOk();
-        await ((AsyncResult<string, int>)(from _ in Ok<string, int>(string.Empty).Async() select Ok<string, int>(string.Empty).Async())).ShouldBeOk();
-
-        await ((AsyncResult<string, int>)(from _ in Ok<string, int>(string.Empty).Async() select Error<string, int>(1).Async())).ShouldBeError(1);
-        await ((AsyncResult<string, int>)(from _ in Error<string, int>(1).Async() select Error<string, int>(2).Async())).ShouldBeError(1);
-        await ((AsyncResult<string, int>)(from _ in Error<string, int>(1).Async() select Ok<string, int>(string.Empty).Async())).ShouldBeError(1);
+        var _1 = ((AsyncResult<decimal, int>)(from _ in Ok<string, int>(string.Empty).Async() select Ok<decimal, int>(decimal.Zero).Async())).ShouldBeOk().Result;
+        var _2 = ((AsyncResult<string, int>)(from _ in Ok<string, int>(string.Empty).Async() select Ok<string, int>(string.Empty).Async())).ShouldBeOk().Result;
+        var _3 = ((AsyncResult<string, int>)(from _ in Ok<string, int>(string.Empty).Async() select Error<string, int>(1).Async())).ShouldBeError(1).Result;
+        var _4 = ((AsyncResult<string, int>)(from _ in Error<string, int>(1).Async() select Error<string, int>(2).Async())).ShouldBeError(1).Result;
+        var _5 = ((AsyncResult<string, int>)(from _ in Error<string, int>(1).Async() select Ok<string, int>(string.Empty).Async())).ShouldBeError(1).Result;
     }
 
     [Property(DisplayName = "AsyncResult SelectMany should project results")]
-    public async void SelectManyProjectsResult()
+    public void SelectManyProjectsResult()
     {
         // leave the type castings to ensure it's not returning a nested Result<Result...>>
 
-        await ((AsyncResult<string, int>)(
+        var _1 = ((AsyncResult<string, int>)(
             from x in Ok<string, int>(string.Empty).Async()
             from y in Ok<string, int>(string.Empty).Async()
             from z in Ok<string, int>(string.Empty).Async()
             select Ok<string, int>("out").Async()))
-            .ShouldBeOk("out");
+            .ShouldBeOk("out").Result;
 
-        await ((AsyncResult<string, int>)(
+        var _2 = ((AsyncResult<string, int>)(
             from x in Ok<string, int>(string.Empty).Async()
             from y in Ok<string, int>(string.Empty).Async()
             from z in Ok<string, int>(string.Empty).Async()
             select Error<string, int>(1).Async()))
-            .ShouldBeError(1);
+            .ShouldBeError(1).Result;
 
-        await ((AsyncResult<string, int>)(
+        var _3 = ((AsyncResult<string, int>)(
             from x in Ok<string, int>(string.Empty).Async()
             from y in Ok<string, int>(string.Empty).Async()
             from z in Error<string, int>(1).Async()
             select Error<string, int>(2).Async()))
-            .ShouldBeError(1);
+            .ShouldBeError(1).Result;
 
-        await ((AsyncResult<decimal, int>)(
+        var _4 = ((AsyncResult<decimal, int>)(
             from x in Ok<string, int>(string.Empty).Async()
             from y in Ok<string, int>(string.Empty).Async()
             from z in Error<string, int>(1).Async()
-            select Error<decimal, int>(2).Async()))
-            .ShouldBeError(1);
+            select Error<decimal, int>(2).Async()))            
+            .ShouldBeError(1).Result;
     }
 }
